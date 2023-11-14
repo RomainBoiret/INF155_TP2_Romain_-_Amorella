d6aeda5060c4cf25d58cb780326d1a2fed49ea43
Romain <romainboiret@gmail.com>
2023-11-14 10:20:50 -0500

    add files

--- TP2 - Circuits/TP2 - Circuits/t_entree.c
+++ TP2 - Circuits/TP2 - Circuits/t_entree.c
@@ -17,3 +17,72 @@
 /*                             DEFINITION DES FONCTIONS                                */
 /***************************************************************************************/
 
+/*==========================================================*/
+//Fonction: T_ENTREE_INIT
+t_entree* t_entree_init(int id, char* nom)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_DESTROY
+void t_entree_destroy(t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_GET_PIN
+t_pin_sortie* t_entree_get_pin(t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_EST_RELIEE
+int t_entree_est_reliee(t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_RESET
+void t_entree_reset(t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_PROPAGER_SIGNAL
+int t_entree_propager_signal(t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_GET_VALEUR
+int t_entree_get_valeur(const t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_GET_ID
+int t_entree_get_id(const t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_GET_NOM
+char* t_entree_get_nom(const t_entree* entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_ENTREE_SERIALISER_LIENS
+void t_entree_serialiser(const t_entree* entree, char* resultat)
+{
+
+}
--- TP2 - Circuits/TP2 - Circuits/t_sortie.c
+++ TP2 - Circuits/TP2 - Circuits/t_sortie.c
@@ -17,3 +17,72 @@
 /*                             DEFINITION DES FONCTIONS                                */
 /***************************************************************************************/
 
+/*==========================================================*/
+//Fonction: T_SORTIE_INIT
+t_sortie* t_sortie_init(int id, char* nom)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_DESTROY
+void t_sortie_destroy(t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_GET_PIN
+t_pin_entree* t_sortie_get_pin(t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_RELIER
+int t_sortie_relier(t_sortie* dest, char* nom_composant, const t_pin_sortie* source)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_EST_RELIEE
+int t_sortie_est_reliee(t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_RESET
+void t_sortie_reset(t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_GET_VALEUR
+int t_sortie_get_valeur(const t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_GET_ID
+int t_sortie_get_id(const t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_GET_NOM
+char* t_sortie_get_nom(const t_sortie* sortie)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_SORTIE_SERIALISER_LIENS
+void t_sortie_serialiser(const t_sortie* sortie, char* resultat)
+{
+
+}
