fbab8446e6e08202cc1e83d51aafd00ebf1a97ac
Romain <romainboiret@gmail.com>
2023-11-14 10:06:58 -0500

    add files

---
+++ TP2 - Circuits/TP2 - Circuits/t_pin_sortie.c
@@ -0,0 +1,81 @@
+/************************************************************************************************************************************************************************************/
+/*  Fichier : T_PIN_SORTIE.C																						                                                                */
+/*  Auteurs : BOIRET Romain   BOIR71300401																		                                                                    */
+/*	          LENGA  Amorella LENA91330301																		                                                                    */
+/*  Date de creation : <14 / 11 / 2023>																		                                                                        */
+/*                                                                                                                                                                                  */
+/*  Ce module contient...                                                                                                                                                           */
+/************************************************************************************************************************************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "t_pin_sortie.h"
+
+/***************************************************************************************/
+/*                             DEFINITION DES FONCTIONS                                */
+/***************************************************************************************/
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_INIT(constructeur)
+t_pin_sortie* t_pin_sortie_init(void)
+{
+	
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_DESTROY(Destructeur)
+void t_pin_sortie_destroy(t_pin_sortie* pin)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_GET_VALEUR (Accesseur)
+int t_pin_sortie_get_valeur(t_pin_sortie* pin)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_SET_VALEUR (Mutateur)
+void t_pin_sortie_set_valeur(t_pin_sortie* pin, int valeur)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_AJOUTER_LIEN
+int t_pin_sortie_ajouter_lien(t_pin_sortie* pin_sortie, const t_pin_entree* pin_entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_SUPPRIMER_LIEN
+void t_pin_sortie_supprimer_lien(t_pin_sortie* pin_sortie, const t_pin_entree* pin_entree)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_EST_RELIEE
+int t_pin_sortie_est_reliee(t_pin_sortie* pin)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: T_PIN_SORTIE_PROPAGER_SIGNAL
+int t_pin_sortie_propager_signal(t_pin_sortie* pin)
+{
+
+}
+
+/*==========================================================*/
+//Fonction: Fonction: T_PIN_SORTIE_RESET
+void t_pin_sortie_reset(t_pin_sortie* pin)
+{
+
+}
