1345968a5bb169f080a3181357c30597cdd19baa
Romain <96925387+RomainBoiret@users.noreply.github.com>
2023-11-21 12:11:06 -0500

    add function t_porte_calculer_sorties (#15)

--- TP2 - Circuits/TP2 - Circuits/main_test_partie1.c
+++ TP2 - Circuits/TP2 - Circuits/main_test_partie1.c
@@ -8,6 +8,7 @@ Programme qui teste l'utilisation des librairies de circuits logique "t_entree"
 #include <assert.h>
 #include "t_entree.h"
 #include "t_sortie.h"
+#include "t_circuit.h"
 
 int main(void)
 {
@@ -116,5 +117,36 @@ int main(void)
 	t_entree_destroy(entree1);
 	t_sortie_destroy(sortie0);
 	t_sortie_destroy(sortie1);
-	return 0;
+	
+	//déclaration des variables
+	int signal[3];		//les 3 valeurs pour les 3 entrées
+	t_circuit* circuit;   //le circuit complet
+	circuit = t_circuit_init();   //Création du circuit
+	construire_circuit(circuit);
+
+	//Vérification de la validité du circuit
+	if (t_circuit_est_valide(circuit)) {
+		printf("Circuit valide!\n");
+	}
+	else {
+		printf("Circuit invalide!\n");
+	}
+
+	//On définit un signal de 3 bits (111)
+	signal[0] = 1;
+	signal[1] = 1;
+	signal[2] = 1;
+	t_circuit_reset(circuit);
+	t_circuit_appliquer_signal(circuit, signal, 3);
+
+	if (t_circuit_propager_signal(circuit)) {
+		printf("Signal propage avec succes.\n");
+		printf("Sortie 0: %d\n", t_sortie_get_valeur(sortie0));
+		printf("Sortie 1: %d\n", t_sortie_get_valeur(sortie1));
+	}
+	else  printf("Erreur lors de la propagation du signal.\n");
+
+	t_circuit_destroy(circuit);
+	system("pause");
+	return EXIT_SUCCESS;
 }
