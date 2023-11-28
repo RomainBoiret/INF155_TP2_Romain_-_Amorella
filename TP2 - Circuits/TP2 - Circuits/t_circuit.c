c0171c02caaf805811d4f8cbf49b148ca623426a
amorella <amorellal@yahoo.com>
2023-11-27 21:40:34 -0500

    Update t_circuit.c

--- TP2 - Circuits/TP2 - Circuits/t_circuit.c
+++ TP2 - Circuits/TP2 - Circuits/t_circuit.c
@@ -20,8 +20,6 @@
 //Fonction: T_CIRCUIT_INIT
 t_circuit* t_circuit_init(void)
 {
-	int i;
-
 	t_circuit* nouv_circuit;
 
 	nouv_circuit = (t_circuit*)malloc(sizeof(t_circuit));
@@ -30,17 +28,17 @@ t_circuit* t_circuit_init(void)
 	nouv_circuit->nb_sorties = NULL;
 	nouv_circuit->nb_portes = NULL;
 
-	for (i = 0; i < MAX_ENTREES; i++)
+	for (int i = 0; i < MAX_ENTREES; i++)
 	{
 		nouv_circuit->entrees[i] = NULL;
 	}
 
-	for (i = 0; i < MAX_SORTIES; i++)
+	for (int i = 0; i < MAX_SORTIES; i++)
 	{
 		nouv_circuit->sorties[i] = NULL;
 	}
 
-	for (i = 0; i < CIRCUIT_MAX_PORTES; i++)
+	for (int i = 0; i < CIRCUIT_MAX_PORTES; i++)
 	{
 		nouv_circuit->portes[i] = NULL;
 	}
@@ -52,10 +50,6 @@ t_circuit* t_circuit_init(void)
 //Fonction: T_CIRCUIT_DESTROY
 void t_circuit_destroy(t_circuit* circuit)
 {
-	free(circuit->entrees);
-	free(circuit->sorties);
-	free(circuit->portes);
-
 	free(circuit);
 }
 
@@ -63,118 +57,95 @@ void t_circuit_destroy(t_circuit* circuit)
 //Fonction: T_CIRCUIT_AJOUTER_PORTE
 t_porte* t_circuit_ajouter_porte(t_circuit* circuit, e_types_portes le_type, int id, char* nom)
 {
-	/*
-Fonction: T_CIRCUIT_AJOUTER_PORTE
-Description: Crée une nouvelle porte et l'ajoute au circuit.
-NDE: Il faut s'assurer qu'on ne dépasse pas le nombre de portes permis (CIRCUIT_MAX_PORTES).
+	//1. Je m'assure que le nombre de portes ne dépasse pas le nombre permis
 
-Paramètres:
-- circuit: Pointeur vers le circuit auquel la porte sera ajoutée
-- type: Type de la porte créée parmi les types de portes prédéfinis (e_type_porte, voir t_porte.h)
-Retour: Pointeur vers la nouvelle porte créée s'il a été possible de la créer. NULL sinon.
-*/
-
-	//1. Je m'assure que le nombre de portes n'est ne dépasse pas le nombre permis
-
-	if (circuit->nb_portes > CIRCUIT_MAX_PORTES)
+	if (circuit->nb_portes < CIRCUIT_MAX_PORTES)
 	{
-		//je crée la nouvelle porte en appelant la fonction qui initialise t_porte
+		//Création nouvelle porte avec appel de fonction 
 
-		circuit->portes[circuit->nb_portes] = t_porte_init(id,le_type,*nom);
+		circuit->portes[circuit->nb_portes] = t_porte_init(id, le_type, nom);
 		circuit->nb_portes++;
-
-		return circuit->portes[circuit->nb_portes-1]; //Retourne Vrai si le lien a bien ete ajoute. //pas certaine de ça
+		//Retourne Vrai si le lien a bien ete ajoute.
+		return (circuit->portes[circuit->nb_portes-1]); 
 	}
 
 	return NULL; //Retourne Faux si le lien n'a pas ete ajoute.
 }
-	
 
 /*==========================================================*/
 //Fonction: T_CIRCUIT_AJOUTER_ENTREE
 t_entree* t_circuit_ajouter_entree(t_circuit* circuit, int id, char* nom)
 {
-/*Fonction: T_CIRCUIT_AJOUTER_ENTREE
-Description : Crée une nouvelle entrée de circuit et l'ajoute au circuit.
-NDE : Il faut s'assurer qu'on ne dépasse pas le nombre d'entrées permis (MAX_ENTREES).
-
-Paramètres :
-	-circuit : Pointeur vers le circuit auquel l'entrée sera ajoutée
-	Retour : Pointeur vers l'entrée créée s'il a été possible de la créer.NULL sinon.*/
-
-	if (circuit->nb_entrees > MAX_ENTREES)
+	if (circuit->nb_entrees < MAX_ENTREES)
 	{
-		//je crée la nouvelle porte en appelant la fonction qui initialise t_porte
+		//Création entrée avec appel de fonction 
 
-		circuit->entrees[circuit->nb_entrees] = t_entree_init(id, *nom);
+		circuit->entrees[circuit->nb_entrees] = t_entree_init(id, nom);
 		circuit->nb_entrees++;
-
-		return circuit->entrees[circuit->nb_entrees - 1]; //Retourne Vrai si le lien a bien ete ajoute. //pas certaine de ça
+		//Retourne Vrai si le lien a bien ete ajoute.
+		return (circuit->entrees[circuit->nb_entrees-1]); 
 	}
-
-	return NULL; //Retourne Faux si le lien n'a pas ete ajoute.
+	//Retourne Faux si le lien n'a pas ete ajoute.
+	return NULL; 
 }
 
 /*==========================================================*/
 //Fonction: T_CIRCUIT_AJOUTER_SORTIE
 t_sortie* t_circuit_ajouter_sortie(t_circuit* circuit, int id, char* nom)
 {
-	if (circuit->nb_sorties > MAX_ENTREES)
+	if (circuit->nb_sorties < MAX_SORTIES)
 	{
-		//je crée la nouvelle porte en appelant la fonction qui initialise t_porte
+		//Création de nouvelle sortie avec appel de fonction
 
-		circuit-> sorties[circuit->nb_sorties] = t_sortie_init(id, *nom);
+		circuit-> sorties[circuit->nb_sorties] = t_sortie_init(id, nom);
 		circuit->nb_sorties++;
-
-		return circuit->sorties[circuit->nb_sorties - 1]; //Retourne Vrai si le lien a bien ete ajoute. //pas certaine de ça
+		//Retourne Vrai si le lien a bien ete ajoute. 
+		return (circuit->sorties[circuit->nb_sorties-1]); 
 	}
-
-	return NULL; //Retourne Faux si le lien n'a pas ete ajoute.
+		//Retourne Faux si le lien n'a pas ete ajoute.
+	return NULL; 
 }
-
 
 /*==========================================================*/
 //Fonction: T_CIRCUITEST_VALIDE
 int t_circuit_est_valide(t_circuit* circuit)
 {
-	/*
-Fonction: T_CIRCUIT_EST_VALIDE
-Description: Vérifie si un circuit donné est un circuit valide. Pour qu'un circuit soit valide
-			 toutes les entrées, portes et sorties du circuit doivent être reliées.
-NDE: Vous utiliserez les fonctions t_entree_est_reliee, t_sortie_est_reliee et t_porte_est_reliee.
-
-Paramètres:
-- circuit: Pointeur vers le circuit dont on veut vérifier la validité
-Retour: (Booléen) Vrai si le circuit est valide, faux sinon.
-*/
-	//appel des 3 fonctions 
-	int valideentree = t_entree_est_reliee(circuit->entrees);
-	int validesortie = t_sortie_est_reliee(circuit->sorties);
-	int valideporte = t_porte_est_reliee(circuit->portes);
-
-	if (valideentree && validesortie && valideporte == 1)
+	for (int i = 0; i < circuit->nb_entrees; i++)
 	{
-		return 1;
+		if (!t_entree_est_reliee(circuit->entrees[i]))
+			return 0;
 	}
-	return 0;
+	
+	for (int i = 0; i < circuit->nb_sorties; i++)
+	{
+		if (!t_sortie_est_reliee(circuit->sorties[i]))
+			return 0;
+	}
+	
+	for (int i = 0; i < circuit->nb_portes; i++)
+	{
+		if (!t_porte_est_reliee(circuit->portes[i]))
+			return 0;
+	}
+
+	return 1;
 }
 
 /*==========================================================*/
 //Fonction: T_CIRCUIT_APPLIQUER_SIGNAL
 int t_circuit_appliquer_signal(t_circuit* circuit, int signal[], int nb_bits)
 {
-	int i;
-
 	if (nb_bits == circuit->nb_entrees)
 	{
 		//On change la valeur du pin de sortie de l'entrée 
-		for (i = 0; i < circuit->nb_entrees; i++)
+		for (int i = 0; i < circuit->nb_entrees; i++)
 		{
-			circuit->entrees[i] = signal[i];
-			signal[i]++;
+			t_pin_sortie_set_valeur(circuit->entrees[i]->pin, signal[i]);
 		}
+
 		return 1;
 	}
+
 	return 0;
 }
 
@@ -182,11 +153,20 @@ int t_circuit_appliquer_signal(t_circuit* circuit, int signal[], int nb_bits)
 //Fonction: T_CIRCUIT_RESET
 void t_circuit_reset(t_circuit* circuit)
 {
-	free(t_entree_reset);
-	free(t_sortie_reset); 
-	free(t_porte_reset);
-	free(circuit);
+	for (int i = 0; i < circuit->nb_entrees; i++)
+	{
+		t_entree_reset(circuit->entrees[i]);
+	}
 
+	for (int i = 0; i < circuit->nb_sorties; i++)
+	{
+		t_sortie_reset(circuit->sorties[i]);
+	}
+
+	for (int i = 0; i < circuit->nb_portes; i++)
+	{
+		t_porte_reset(circuit->portes[i]);
+	}
 }
 
 /*==========================================================*/
